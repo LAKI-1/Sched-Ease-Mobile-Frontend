064c8edc6b3d800cae2beedf5a5f15e41fb36b31
thishakya21 <thishakya21@gmail.com>
2025-02-10 08:07:11 +0530

    Initial design of the dashboard
    Includes:
    - Dark blue and white palette
    - Images

---
+++ front_end_schedease/lib/dashboard.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:front_end_schedease/widgets/schedule_card.dart';
+// import 'widget/schedule_card.dart';
+
+class DashBoard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white, //Sets background color to white
+      bottomNavigationBar: _buildBottomNavBar(), //Calls the function to build the bottom nav bar
+      body: SafeArea(  //UI avoids device notches and status bars
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0), //Adding horizontal padding to the whole screen
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, //Aligns children to the left side
+              children: [
+                SizedBox(height: 20),     //Adds spacing at the top
+                _buildGreetingSection(),  //Calls function to build the greeting section
+                SizedBox(height: 20),     //Adds spacing
+                _buildTimeAndQuoteCard(), //Calls function to build time & quotes UI
+                Spacer(), //Pushes this section to the bottom
+                _buildScheduleSection(),  //Calls function to build the schedule section UI
+                SizedBox(height: 20),
+
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
+  //Greeting Section ("Good Morning, Student!")
+  Widget _buildGreetingSection(){
+    return Row(   //Creating a horizontal row
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,  //Distributes the items to both ends
+      children: [
+        //DP
+        CircleAvatar( //Displays a circular profile picture of the student
+          radius: 40, //Size
+          backgroundImage:AssetImage('assets/avatar.jpg'), //Place holder image
+        ),
+        //Greeting
+        Column(
+              crossAxisAlignment: CrossAxisAlignment.end, //Align text to the right
+              children: [
+                Text(
+                  "Good Morning,",
+                  style: TextStyle(fontSize: 14, color: Colors.grey.shade600), //Smaller grey text for greeting
+                ),
+                Text(
+                  "Sienna Riverly!",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+
+      ],
+
+    );
+  }
+
+  //Time & Quote Card
+  Widget _buildTimeAndQuoteCard(){
+    return Container(
+        padding: EdgeInsets.all(20), //Adds padding inside the card
+        width: double.infinity,
+        height:150,
+        decoration: BoxDecoration(
+          color: Color(0xFF3C5A7D), //Dark blue background
+          borderRadius: BorderRadius.circular(20), //Rounded corner
+        ),
+        child:Stack(
+          children:[
+
+            //Time: Top left
+            Positioned(
+              left: 10, //left
+              top: 10,  //top
+              child: Text(
+                "08:00 AM",
+                style:TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            // Quote: Bottom Right
+            Positioned(
+              right:5,
+              bottom:2,
+              child:Text(
+                "DREAM.PLAN.DO.",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight:FontWeight.bold,
+                ),
+              )
+            )
+          ]
+        )
+    );
+  }
+
+
+  //Today's Schedule
+  Widget _buildScheduleSection() {
+    return Container(
+      width: double.infinity, // Container spans full width
+      padding: EdgeInsets.symmetric(horizontal: 20), //Adding horizontal padding
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, //Aligns the children to left side
+        children: [
+          //Title
+          Text(
+              "Today's Schedule",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+
+          SizedBox(height: 15), //Adds space
+
+          //First Row: Two schedule cards
+          Row(
+            children: [
+              //Schedule card 1: Feedback Session
+              Expanded(
+                child: ScheduleCard(
+                    title: "Feedback Session",
+                    time: "10:00 - 10:30",
+                    color: Color(0xFFE1F1FD),
+                ),
+
+              ),
+
+              SizedBox(width:10), //Adds space
+
+              //Schedule card 2: Coursework
+              Expanded(
+                child: ScheduleCard(
+                    title: "SDGP CW I Submission",
+                    time: "13:30",
+                    color: Color(0xFFFBFBFB),
+                    imagePath:'assets/cw.png'
+                ),
+              ),
+            ],
+          ),
+
+          SizedBox(height:10), //Adds space
+
+          //Schedule card 3: Lecture
+          ScheduleCard(
+              title: "Database Lecture",
+              time: "14:00 - 16:00",
+              color: Color(0xFFFBFBFB),
+              imagePath:'assets/lec.png'
+          ),
+
+          SizedBox(height: 20),
+          _buildViewMoreButton(), //Calls function to add in the "View Schedule Button"
+
+        ],
+      ),
+    );
+
+  }
+
+  //View More Button
+  Widget _buildViewMoreButton() {
+    return Center(
+      child: Container(
+        height: 60, //Sets a fixed height
+        width: double.infinity, //Buttons spans to full width
+        alignment: Alignment.center, //Centers text in button
+        decoration: BoxDecoration(
+          color: Color(0xFF1A365D), //BG color of the button
+          borderRadius: BorderRadius.circular(15), //Rounded corners
+        ),
+        child: Text(
+          "View Schedule",
+          style: TextStyle(color: Colors.white,fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+
+  );
+}
+
+
+  // ⚡ Bottom Navigation Bar
+  Widget _buildBottomNavBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"), // Home icon.
+        BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: "Schedule"), // Calendar icon.
+        BottomNavigationBarItem(icon: Icon(Icons.message), label: "Chat"), // Chat icon.
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"), // Profile icon.
+      ],
+      selectedItemColor: Colors.blue, // Selected item color.
+      unselectedItemColor: Colors.grey, // Unselected item color.
+    );
+  }
+}
