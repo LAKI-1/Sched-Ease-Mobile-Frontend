bc994a72ccd4abf4b572dc3c00a87bc41d56b2a4
thishakya21 <thishakya21@gmail.com>
2025-02-25 10:10:47 +0530

    Improved Schedule Page with dynamic values and better design

--- front_end_schedease/lib/features/schedule_page.dart
+++ front_end_schedease/lib/features/schedule_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; //dependency for date formatting
+import 'package:front_end_schedease/widgets/navbar.dart';
 
 void main(){
   runApp(MaterialApp(
@@ -88,14 +89,18 @@ class _SchedulePageState extends State<SchedulePage>{
       body: SafeArea(
           child: Column(
             children: [
+              Container(
+                height: 20, //Adds space between top and calendar
+                color: Colors.white,
+              ),
               _buildCalendarSection(),
-
               Expanded(
                   child: _buildScheduleContent(),
               ),
             ],
           ),
       ),
+      //bottomNavigationBar: _buildBottomNavBar(),
     );
   }
 
@@ -103,9 +108,10 @@ class _SchedulePageState extends State<SchedulePage>{
   Widget _buildCalendarSection(){
     return Container(
       padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(top: 20, left: 15,right: 15),
       decoration: BoxDecoration(
         color: Color(0xFF3C5A7D), //Bg color of the calendar
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        borderRadius: BorderRadius.all(Radius.circular(60)),
       ),
       child: Column(
         children: [
@@ -121,10 +127,10 @@ class _SchedulePageState extends State<SchedulePage>{
 
               //Current Month and Year
               Text(
-                DateFormat('MMMM yyyy').format(_currentMonth),
+                DateFormat('MMMM').format(_currentMonth),
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 20,
+                  fontSize: 24,
                   fontWeight: FontWeight.w500,
                 ),
               ),
@@ -140,7 +146,7 @@ class _SchedulePageState extends State<SchedulePage>{
 
           //Displaying Week with PageView
           SizedBox(
-            height: 80,
+            height: 100,
             child: PageView.builder(
               controller: _weekPageController,
               onPageChanged: (page) {
@@ -172,6 +178,8 @@ class _SchedulePageState extends State<SchedulePage>{
               },
             ),
           ),
+          SizedBox(height: 20),
+          _buildBookSessionButton(),
         ],
       ),
     );
@@ -196,11 +204,11 @@ class _SchedulePageState extends State<SchedulePage>{
         });
       },
       child: Container(
-        width: 60,
-        padding: EdgeInsets.symmetric(vertical: 10),
+        width: 65,
+        padding: EdgeInsets.symmetric(vertical: 12),
         decoration: BoxDecoration(
           color: isSelected ? Colors.white : Colors.transparent,
-          borderRadius: BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(20),
         ),
         child: Column(
           children: [
@@ -216,7 +224,7 @@ class _SchedulePageState extends State<SchedulePage>{
                 fontSize: 16,
               ),
             ),
-            SizedBox(height: 5),
+            SizedBox(height: 8),
 
             //Displaying date
             Text(
@@ -227,11 +235,44 @@ class _SchedulePageState extends State<SchedulePage>{
                     : isCurrentMonth
                     ? Colors.white
                     : Colors.white.withOpacity(0.5),
-                fontSize: 20,
+                fontSize: 22,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _buildBookSessionButton() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 15),
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 0,
+            blurRadius: 10,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+
+      child: Center(
+        child: Text(
+          'Book a Feedback Session',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
         ),
       ),
     );
@@ -244,41 +285,14 @@ class _SchedulePageState extends State<SchedulePage>{
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          //Book Session Button
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 15),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 1,
-                  blurRadius: 5,
-                ),
-              ],
-            ),
-            child: Center(
-              child: Text(
-                'Book a Feedback Session',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-
-          //Display the selected date large
           Text(
-            DateFormat('EEEE, d\'th\'').format(_selectedDate),
+            '${DateFormat('EEEE, d').format(_selectedDate)}st',
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
             ),
           ),
+
           SizedBox(height: 20),
 
           //List of schedule items
@@ -355,335 +369,6 @@ class _SchedulePageState extends State<SchedulePage>{
         ),
       );
     }
-}
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class SchedulePage extends StatefulWidget {
-//   @override
-//   _SchedulePageState createState() => _SchedulePageState();
-// }
-//
-// class _SchedulePageState extends State<SchedulePage> {
-//   // Initialize controllers and date variables
-//   late PageController _weekPageController;
-//   late DateTime _selectedDate;
-//   late DateTime _currentMonth;
-//   int _currentWeekPage = 1000; // Start from middle for infinite scroll
-//
-//   // Sample schedule data
-//   final List<Map<String, dynamic>> scheduleItems = [
-//     {
-//       'time': '10.00',
-//       'title': 'Session With Supervisor(Mr. John)',
-//       'duration': '10.00 - 12.00',
-//       'backgroundColor': Colors.blue.withOpacity(0.1),
-//     },
-//     {
-//       'time': '13.00',
-//       'title': 'CS-12 Group Meeting',
-//       'duration': '13.00 - 14.00',
-//       'backgroundColor': Colors.red.withOpacity(0.1),
-//     },
-//   ];
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _selectedDate = DateTime.now();
-//     _currentMonth = DateTime(_selectedDate.year, _selectedDate.month);
-//     _weekPageController = PageController(initialPage: _currentWeekPage);
-//   }
-//
-//   @override
-//   void dispose() {
-//     _weekPageController.dispose();
-//     super.dispose();
-//   }
-//
-//   // Get the first date of the week
-//   DateTime _getFirstDayOfWeek(DateTime date) {
-//     return date.subtract(Duration(days: date.weekday - 1));
-//   }
-//
-//   // Get the week days
-//   List<DateTime> _getDaysInWeek(DateTime date) {
-//     DateTime firstDayOfWeek = _getFirstDayOfWeek(date);
-//     return List.generate(5, (index) => firstDayOfWeek.add(Duration(days: index)));
-//   }
-//
-//   // Handle month changes
-//   void _handleMonthChange(bool next) {
-//     setState(() {
-//       if (next) {
-//         _currentMonth = DateTime(_currentMonth.year, _currentMonth.month + 1);
-//       } else {
-//         _currentMonth = DateTime(_currentMonth.year, _currentMonth.month - 1);
-//       }
-//       _selectedDate = _currentMonth;
-//
-//       // Update the page view to show the first week of the new month
-//       DateTime firstDayOfMonth = _getFirstDayOfWeek(_currentMonth);
-//       int pageDiff = next ? 4 : -4; // Approximate weeks in a month
-//       _currentWeekPage += pageDiff;
-//       _weekPageController.jumpToPage(_currentWeekPage);
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       body: SafeArea(
-//         child: Column(
-//           children: [
-//             _buildCalendarSection(),
-//             Expanded(
-//               child: _buildScheduleContent(),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildCalendarSection() {
-//     return Container(
-//       padding: EdgeInsets.all(20),
-//       decoration: BoxDecoration(
-//         color: Color(0xFF6B8E9B),
-//         borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
-//       ),
-//       child: Column(
-//         children: [
-//           // Month navigation row
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               IconButton(
-//                 icon: Icon(Icons.chevron_left, color: Colors.white),
-//                 onPressed: () => _handleMonthChange(false),
-//               ),
-//               Text(
-//                 DateFormat('MMMM yyyy').format(_currentMonth),
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//               IconButton(
-//                 icon: Icon(Icons.chevron_right, color: Colors.white),
-//                 onPressed: () => _handleMonthChange(true),
-//               ),
-//             ],
-//           ),
-//           SizedBox(height: 20),
-//           // Week view
-//           SizedBox(
-//             height: 80,
-//             child: PageView.builder(
-//               controller: _weekPageController,
-//               onPageChanged: (page) {
-//                 setState(() {
-//                   int weekDiff = page - _currentWeekPage;
-//                   _currentWeekPage = page;
-//                   DateTime newDate = _selectedDate.add(Duration(days: weekDiff * 7));
-//                   _selectedDate = newDate;
-//                   _currentMonth = DateTime(newDate.year, newDate.month);
-//                 });
-//               },
-//               itemBuilder: (context, page) {
-//                 int weekDiff = page - _currentWeekPage;
-//                 DateTime weekStart = _selectedDate.add(Duration(days: weekDiff * 7));
-//                 List<DateTime> days = _getDaysInWeek(weekStart);
-//
-//                 return Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                   children: days.map((date) => _buildDayItem(date)).toList(),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget _buildDayItem(DateTime date) {
-//     bool isSelected = date.year == _selectedDate.year &&
-//         date.month == _selectedDate.month &&
-//         date.day == _selectedDate.day;
-//     bool isCurrentMonth = date.month == _currentMonth.month;
-//
-//     return GestureDetector(
-//       onTap: () {
-//         setState(() {
-//           _selectedDate = date;
-//           _currentMonth = DateTime(date.year, date.month);
-//         });
-//       },
-//       child: Container(
-//         width: 60,
-//         padding: EdgeInsets.symmetric(vertical: 10),
-//         decoration: BoxDecoration(
-//           color: isSelected ? Colors.white : Colors.transparent,
-//           borderRadius: BorderRadius.circular(15),
-//         ),
-//         child: Column(
-//           children: [
-//             Text(
-//               DateFormat('E').format(date).substring(0, 3),
-//               style: TextStyle(
-//                 color: isSelected
-//                     ? Colors.black
-//                     : isCurrentMonth
-//                     ? Colors.white
-//                     : Colors.white.withOpacity(0.5),
-//                 fontSize: 16,
-//               ),
-//             ),
-//             SizedBox(height: 5),
-//             Text(
-//               date.day.toString(),
-//               style: TextStyle(
-//                 color: isSelected
-//                     ? Colors.black
-//                     : isCurrentMonth
-//                     ? Colors.white
-//                     : Colors.white.withOpacity(0.5),
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildScheduleContent() {
-//     return Container(
-//       padding: EdgeInsets.all(20),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.stretch,
-//         children: [
-//           // Feedback session button
-//           Container(
-//             padding: EdgeInsets.symmetric(vertical: 15),
-//             decoration: BoxDecoration(
-//               color: Colors.white,
-//               borderRadius: BorderRadius.circular(15),
-//               boxShadow: [
-//                 BoxShadow(
-//                   color: Colors.grey.withOpacity(0.1),
-//                   spreadRadius: 1,
-//                   blurRadius: 5,
-//                 ),
-//               ],
-//             ),
-//             child: Center(
-//               child: Text(
-//                 'Book a Feedback Session',
-//                 style: TextStyle(
-//                   fontSize: 16,
-//                   fontWeight: FontWeight.w600,
-//                   color: Colors.black,
-//                 ),
-//               ),
-//             ),
-//           ),
-//           SizedBox(height: 30),
-//           // Selected date display
-//           Text(
-//             DateFormat('EEEE, d\'th\'').format(_selectedDate),
-//             style: TextStyle(
-//               fontSize: 24,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//           SizedBox(height: 20),
-//           // Schedule items list
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: scheduleItems.length,
-//               itemBuilder: (context, index) {
-//                 final item = scheduleItems[index];
-//                 return _buildScheduleItem(item);
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget _buildScheduleItem(Map<String, dynamic> item) {
-//     return Padding(
-//       padding: EdgeInsets.symmetric(vertical: 10),
-//       child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           SizedBox(
-//             width: 50,
-//             child: Text(
-//               item['time'],
-//               style: TextStyle(
-//                 fontSize: 14,
-//                 color: Colors.grey[600],
-//               ),
-//             ),
-//           ),
-//           SizedBox(width: 10),
-//           Expanded(
-//             child: Container(
-//               padding: EdgeInsets.all(15),
-//               decoration: BoxDecoration(
-//                 color: item['backgroundColor'],
-//                 borderRadius: BorderRadius.circular(15),
-//               ),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                     item['title'],
-//                     style: TextStyle(
-//                       fontSize: 16,
-//                       fontWeight: FontWeight.w600,
-//                     ),
-//                   ),
-//                   SizedBox(height: 5),
-//                   Text(
-//                     item['duration'],
-//                     style: TextStyle(
-//                       fontSize: 14,
-//                       color: Colors.grey[600],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
