4994c46cd4203dbe21f3af1e6816c51a32a7e921
Kavindya Bandara <kavindya456789@gmail.com>
2025-03-01 11:58:45 +0530

    Added the logo image to the splash screen center. Implemented sign-in button with circular arrow. Updated sign-in button to trigger login screen.

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,10 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sign_in_screen/ui/screens/login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Splash Screen")));
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  colors: [
+                    Color.fromARGB(255, 154, 200, 216),
+                    Color.fromARGB(255, 236, 231, 226),
+                    Colors.white,
+                  ],
+                  stops: [0.0, 0.2, 1.0],
+                ),
+              ),
+            ),
+          ),
+
+          Positioned.fill(
+            child: Image.asset(
+              'assets/rectangle.png',
+              fit: BoxFit.cover,
+              opacity: const AlwaysStoppedAnimation(1),
+            ),
+          ),
+
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  70.verticalSpace,
+                  Image.asset(
+                    'assets/logo.png',
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    fit: BoxFit.contain,
+                  ),
+
+                  const SizedBox(height: 40),
+                  50.verticalSpace,
+
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF008080),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 3,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 16,
+                          ),
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                        InkWell(
+                          borderRadius: BorderRadius.circular(30),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
+                          },
+
+                          child: Container(
+                            width: 24,
+                            height: 24,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Color(0xFFA9A9A9),
+                                width: 1,
+                              ),
+                            ),
+
+                            child: const Center(
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.black87,
+                                size: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
