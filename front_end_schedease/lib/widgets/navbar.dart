20540a29bf101c0009f2f22bdbd4334e4d2b52c2
thishakya21 <thishakya21@gmail.com>
2025-03-03 13:50:04 +0530

    Fixed an error with navigation bar

--- front_end_schedease/lib/widgets/navbar.dart
+++ front_end_schedease/lib/widgets/navbar.dart
@@ -1,138 +1,167 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatefulWidget{ //Stateful widget allows to track which item is currently selected
-  final int initiallySelectedIndex; //The index selected when the widget
-  final Function(int) onItemSelected; //A function that gets called when a new item is selected
+class BottomNavBar extends StatefulWidget {
+  final int initiallySelectedIndex;
+  final Function(int) onItemSelected;
 
-  //Constructor that needs a callback function and initial index default to 0
   BottomNavBar({
     this.initiallySelectedIndex = 0,
     required this.onItemSelected,
   });
+
   @override
   _BottomNavBarState createState() => _BottomNavBarState();
-
 }
 
-//The state class for the BottomNavBar
-class _BottomNavBarState extends State<BottomNavBar>{
-  late int selectedIndex; // Stores the currently selected index
+class _BottomNavBarState extends State<BottomNavBar> with SingleTickerProviderStateMixin {
+  late int selectedIndex;
+  late AnimationController _animationController;
+
+  // Mint green color for selected items
+  final Color colorAroundNavItem = Color(0xFF3C5A7D);
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    //Initialise the index selected with the value provided in the widget constructor
     selectedIndex = widget.initiallySelectedIndex;
+
+    // Initialize animation controller for subtle hover effects
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
   }
 
   @override
-  Widget build(BuildContext context){
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      //Outer container: Holds entire nav bar
       decoration: BoxDecoration(
-        color: Colors.white,    //Bg color of the navbar
-        borderRadius: BorderRadius.circular(40), //Rounded corners
-        boxShadow:[
-          //Adding a shadow
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            blurRadius: 10,
-            offset: Offset(0, 2), // Coordinates of where the shadow appear (below navbar)
-          )
-        ]
+            color: Colors.grey.withOpacity(0.15),
+            blurRadius: 15,
+            spreadRadius: 1,
+            offset: Offset(0, 2),
+          ),
+        ],
       ),
-
-      //Margins that create space between navbar and the schedule cards
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround, //Distributing the space around the row evenly
-        children:[
-          //Home item
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
           _buildNavItem(
-            iconPath: 'assets/home.png', //Home icon being outlined
-            label: 'Home', //Text
-            index: 0, //Indexed position
+            icon: Icons.home_rounded,
+            label: 'Home',
+            index: 0,
           ),
-
-          //Plan item
           _buildNavItem(
-            iconPath: 'assets/schedule.png', //Home icon being outlined
-            label: 'Plan', //Text
-            index: 1, //Indexed position
+            icon: Icons.calendar_month_rounded,
+            label: 'Book',
+            index: 1,
           ),
-
-          //Chat item
           _buildNavItem(
-            iconPath: 'assets/chat.png', //Home icon being outlined
-            label:'Chat' , //Text
-            index: 2, //Indexed position
+            icon: Icons.folder_copy_rounded,
+            label: 'Log',
+            index: 2,
           ),
-
-          //Feedback item
           _buildNavItem(
-            iconPath: 'assets/feedback.png', //Home icon being outlined
-            label: 'Help', //Text
-            index: 3, //Indexed position
+            icon: Icons.chat_rounded,
+            label: 'Chat',
+            index: 3,
           ),
-        ]
+        ],
       ),
     );
   }
 
-  //Method to build individual navigation items
   Widget _buildNavItem({
-    required String iconPath, //Path of the icon image
-    required String label,    //Text below the icon
-    required int index,       //Position of the icon
-
-
+    required IconData icon,
+    required String label,
+    required int index,
   }) {
-    //Check if this item is the currently selected one
     bool isSelected = selectedIndex == index;
 
-    //Making the icons tappable
-    return GestureDetector(
-      onTap: (){      //Has what happens when you tap the icon
-        setState(() { //Updates to the newly selected index
-          selectedIndex = index;
-        });
-
-
-        widget.onItemSelected(index);
-      },
-      child: Container( //Container for customising each item
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-
-        decoration: BoxDecoration(
-          color: isSelected ? Color(0xFFEBEEFF) : Colors.transparent, //Bg color of selected icon
-          borderRadius: BorderRadius.circular(20),
-        ),
-
-        child: Column(
-          mainAxisSize: MainAxisSize.min, //Column height
-          children: [
-            Image.asset(
-              iconPath,
-              width: 24,
-              height: 24,
-              color: isSelected ? Colors.blue.shade700 : Colors.grey.shade600, //deciding the icon color based on selection stae
-            ),
-
-            SizedBox(height:2), //Space between the icon and text
-
-            Text(
-              label,
-              style: TextStyle(
-                color: isSelected ? Colors.blue.shade700 : Colors.grey.shade600,
-                fontSize : 10,
-                fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+          _animationController.reset();
+          _animationController.forward();
+          widget.onItemSelected(index);
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.transparent,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Animated container for the icon
+              AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                padding: EdgeInsets.all(isSelected ? 12 : 10),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isSelected ? colorAroundNavItem : Colors.grey.withOpacity(0.1),
+                  boxShadow: isSelected
+                      ? [
+                    BoxShadow(
+                      color: colorAroundNavItem.withOpacity(0.3),
+                      blurRadius: 8,
+                      spreadRadius: 1,
+                      offset: Offset(0, 2),
+                    )
+                  ]
+                      : null,
+                ),
+                child: Icon(
+                  icon,
+                  color: isSelected ? Colors.white : Colors.grey.shade600,
+                  size: 22,
+                ),
               ),
-            ),
-          ],
+
+              SizedBox(height: 6),
+
+              // Label with animation
+              AnimatedDefaultTextStyle(
+                duration: Duration(milliseconds: 200),
+                style: TextStyle(
+                  fontSize: 11,
+                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+                  color: isSelected ? colorAroundNavItem : Colors.grey.shade600,
+                ),
+                child: Text(label),
+              ),
+
+              // Animated indicator dot
+              AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                height: 4,
+                width: isSelected ? 20 : 0,
+                margin: EdgeInsets.only(top: 4),
+                decoration: BoxDecoration(
+                  color: isSelected ? colorAroundNavItem : Colors.transparent,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
