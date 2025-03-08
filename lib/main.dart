a722d25a60157085326d501d8f4df05a87e11ef2
Kavindya Bandara <kavindya456789@gmail.com>
2025-03-08 14:44:19 +0530

    Add Supabase initialization with configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sign_in_screen/core/utils/route_utils.dart';
 import 'package:sign_in_screen/ui/screens/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://niwzewmxvdnbmkfxxxbk.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pd3pld214dmRuYm1rZnh4eGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNjIzNjksImV4cCI6MjA1NjczODM2OX0.PiChm9zF2rmfABh6y02VHP6amzI3hnj2fiuYzp_W4Fw',
+  );
   runApp(const SignInScreen());
 }
 
